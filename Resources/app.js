54da52ca3ea35c00d019ef8bf4041d7942df6d7e
AppWerft <rs@hamburger-appwerft.de>
2014-08-01 15:17:12 +0200

        

--- Resources/app.js
+++ Resources/app.js
@@ -1,42 +1,23 @@
 //
 var win1 = Ti.UI.createWindow({
-	title : 'Accountmanager',
-	backgroundColor : '#fff'
+	title : 'Ti.AccountManager',
+	backgroundColor : '#fff',
+	exitOnClose : true,
+	fullscreen : trues
 });
-
-var label1 = Titanium.UI.createLabel({
-	color : '#999',
-	text : 'I am Window 1',
-	font : {
-		fontSize : 20,
-		fontFamily : 'Helvetica Neue'
-	},
-	textAlign : 'center',
-	width : 'auto'
-});
-
-win1.add(label1);
 
 win1.open();
 
 var AM = require('org.bcbhh');
-
 var accounts = AM.getAccounts();
-
 console.log(accounts);
-Ti.UI.createNotification({
-	message : accounts.length + ' found'
-}).show();
+
 var options = [];
 for (var i = 0; i < accounts.length; i++) {
 	options.push(accounts[i].accountType);
 }
 var accountlist = Ti.UI.createOptionDialog({
 	options : options,
-	title : 'Wähle Dein Konto!'
+	title : 'Choose your account!'
 });
 accountlist.show();
-accountlist.addEventListener('click', function(_e) {
-
-});
-
